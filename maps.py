92b1b2c6a56738272c71fa539af6d230369cf4da
Giulio Ungaretti <giulioungaretti@me.com>
2014-08-30 12:09:23 +0200

    map overlay

--- maps.py
+++ maps.py
@@ -1,3 +1,6 @@
 import folium
-map_osm = folium.Map(location=[55.6761, 12.5683])
-map_osm.create_map(path='cph.html')
+# data json file for the overlay
+topo = './data/taxzone.json'
+map = folium.Map(location=[55.6761, 12.5683])
+map.geo_json(geo_path=topo)
+map.create_map(path='cph.html')
