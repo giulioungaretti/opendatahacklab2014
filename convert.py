18046a114ebffd89bf34e1d559edd23dc698e90f
Guen P <guen@nbi.dk>
2014-09-06 22:59:45 +0200

    singles*100

--- convert.py
+++ convert.py
@@ -1,12 +1,10 @@
 import json
 
-
-
-  def toJson(self):
-        '''
-        save json file to disk as a json list of list
-        i.e. [{"keyonval":count }..]
-        '''
-        dictionary = [dict(zip(self.map_df.names, self.map_df.counts))]
-        with open('data/{0}_data.json'.format(self.dealer), 'w') as outfile:
-            json.dump(dictionary, outfile)
+def toJson(self):
+    '''
+    save json file to disk as a json list of list
+    i.e. [{"keyonval":count }..]
+    '''
+    dictionary = [dict(zip(self.map_df.names, self.map_df.counts))]
+    with open('data/{0}_data.json'.format(self.dealer), 'w') as outfile:
+        json.dump(dictionary, outfile)
