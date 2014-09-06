0d0e875bf4549df1832305096b575939b6d2c568
Guen P <guen@nbi.dk>
2014-09-06 23:28:12 +0200

    bla

--- maps.js
+++ maps.js
@@ -165,7 +165,7 @@ function map_me(map, data) {
 
 function get_dealer_map(e) {
     dealer = e.srcElement.value
-    var data = 'data/' + dealer + '_data.json'
+    var data = 'data/' + dealer + '.json'
     if (dealer) {
         try {
             map.removeLayer(geojson)
