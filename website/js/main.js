6ad43be555e56abe2be74355cabcf63f96ff0a06
Giulio Ungaretti <giulioungaretti@me.com>
2014-10-19 20:55:52 +0200

    ready to rock

--- website/js/main.js
+++ website/js/main.js
@@ -30,11 +30,12 @@ L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png', {
     id: "giulioungaretti.k09mbfjd"
 }).addTo(map);
 
+var geojsonLayer;
 
-function drawMap(data, geojson) {
 
+function drawMap(data, geojson, data_raw) {
     // d3 loading effect
-    // d3.select(".spinner").remove().transition().delay(100)
+     d3.select(".spinner").remove().transition().delay(100)
 
     // set domain of color maps
     var min = d3.min(d3.values(data)),
@@ -142,8 +143,7 @@ function drawMap(data, geojson) {
         return val;
     }
 
-    // plug in topojson
-    var geojsonLayer = L.geoJson(geojson, {
+    geojsonLayer = L.geoJson(geojson, {
         style: feature_style,
         onEachFeature: onEachFeature
     }).addTo(map);
@@ -152,6 +152,8 @@ function drawMap(data, geojson) {
         if (props) {
             //If value exists.
             var val = matchKey(props.id, data);
+            // do your magic here Henri
+            console.log(data_raw)
         } else {
             var val = 0
         }
@@ -161,85 +163,52 @@ function drawMap(data, geojson) {
 
 };
 
-//fetch data first
+// define data path
 dataUrl = "./data/complete_dataset.json"
-// dataUrl = "./data/final_index.json"
-
 
 function parse_data(data, weights) {
+    // TODO: separate data loading and aggregation.
     var x = new Object();
     var y = new Object();
     data.forEach(
         function(d) {
-            x[d.id] = d3.values(d).slice(1,3)
+            x[d.id] = d3.values(d).slice(1, 3)
             var temp = 1
             for (var i = 0; i < x[d.id].length; i++) {
-                temp += parseFloat(x[d.id][i]) *  parseFloat(weights[i])
+                temp += parseFloat(x[d.id][i]) * parseFloat(weights[i])
             };
             y[d.id] = temp
             // d.ages + d.cars  + d.bikes + d.digging  + d.poi + d.female_singles
 
         })
-    // console.log(y)
-    return y
+    // returns raw data, aggregated data
+    return [x, y]
 }
 
 
-var weights = [1,2] // , 1, 1, 1, 1, 1, 1, 1, 1]
-
-
-
-// // add d3 slider
-
-// // create globals
+var weights = [1, 2] // , 1, 1, 1, 1, 1, 1, 1, 1]
 
 
 var names = ["#slider1"] // , "#slider1", "#slider1", "#slider1", "#slider1", "#slider1" ]
 
 d3.select('#slider1').call(d3.slider().on("slide", function(evt, value) {
     weights[0] = value
-    d3.json(dataUrl, function(error, data ) {
-        console.log(weights)
+
+    d3.json(dataUrl, function(error, data) {
         if (error) {
             console.log(error)
         } else {
-             //get map
-            d3.json( "./data/taxzone.json", function(mapData) {
-            data = parse_data(data, weights)
-            drawMap(data, mapData);}
-            )
+            //get map
+            d3.json("./data/taxzone.json", function(mapData) {
+                data = parse_data(data, weights)
+                try {
+                    map.removeLayer(geojsonLayer)
+                } catch (err) {
+                    console.log(err)
+                };
+
+                drawMap(data[1], mapData, data[0]);
+            })
         }
     });
-    // console.log(weights  )
 }));
-
-// //value2
-// d3.select('#slider2').call(d3.slider().on("slide", function(evt, value) {
-//     val2 = value
-//     console.log(value)
-// }));
-
-// //value3
-// d3.select('#slider3').call(d3.slider().on("slide", function(evt, value) {
-//     val3 = value
-// }));
-// //value3
-// d3.select('#slider3').call(d3.slider().on("slide", function(evt, value) {
-//     val3 = value
-// }));
-// //value3
-// d3.select('#slider3').call(d3.slider().on("slide", function(evt, value) {
-//     val3 = value
-// }));
-// //value3
-// d3.select('#slider3').call(d3.slider().on("slide", function(evt, value) {
-//     val3 = value
-// }));
-// //value3
-// d3.select('#slider3').call(d3.slider().on("slide", function(evt, value) {
-//     val3 = value
-// }));
-// //value3
-// d3.select('#slider3').call(d3.slider().on("slide", function(evt, value) {
-//     val3 = value
-// }));
